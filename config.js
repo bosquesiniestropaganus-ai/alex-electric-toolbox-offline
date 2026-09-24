6033aa7235720003245e1b2b3ea0ba7eb2d21838
Alex Toolbox <noreply@users.noreply.github.com>
2026-09-24 08:44:47 -0300

    Connect V4.5 PWA to hybrid backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ALEX_HYBRID_CONFIG={
   version:'4.5.0',
   appUrl:'https://bosquesiniestropaganus-ai.github.io/alex-electric-toolbox-offline/',
-  backendUrl:'https://script.google.com/macros/s/AKfycbwozUq1i3btzEJa94T_vZkFw6HU826731ciMGXhYWkZwnfkEV4yJYKQ0Ky7CZI2BK7d/exec',
+  backendUrl:'https://script.google.com/macros/s/AKfycbxXrJ4SEeh8yi3SQLEfy1ghVIWgIOl3hdhLq4cEACLeBTwFJhj9E80L4YBlHvLDOGV0/exec',
   bridgeTimeoutMs:45000
 };
